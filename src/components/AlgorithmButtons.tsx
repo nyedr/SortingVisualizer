7f598de06f50db557b748ba90ad617640c42deb6
EidanGar <eidangarcia787i@gmail.com>
2023-06-11 19:01:48 -0400

    Fixed algo buttons bug by removing type casting

--- src/components/AlgorithmButtons.tsx
+++ src/components/AlgorithmButtons.tsx
@@ -2,10 +2,10 @@ import { Dispatch, SetStateAction } from "react";
 import { Algorithm } from "../App";
 import { AlgoButton, AlgoBtnGroup } from "./componentStyles/AlgoButtonStyles";
 
-const SortingAlgorithmsArray: Algorithm[] = [
-  Algorithm.QUICK,
-  Algorithm.BUBBLE,
-  Algorithm.MERGE
+const SortingAlgorithmsArray = [
+  "Quick",
+  "Bubble",
+  "Merge"
 ];
 
 interface SortingAlgoBtnsProps {
@@ -13,29 +13,42 @@ interface SortingAlgoBtnsProps {
 }
 
 interface SortingAlgoBtnProps {
-  algorithm: Algorithm;
+  algorithm: string;
   index: number;
+  setAlgorithm: Dispatch<SetStateAction<Algorithm>>;
 }
 
-const SortingAlgorithmBtns = ({ setAlgorithm }: SortingAlgoBtnsProps) => {
-  const changeAlgo = (algorithm: Algorithm) => {
-    setAlgorithm(algorithm);
+const SortingAlgorithmBtn = ({ algorithm, index, setAlgorithm }: SortingAlgoBtnProps) => {
+  const changeAlgo = (algorithm: string) => {
+    switch(algorithm) {
+      case "Quick":
+        setAlgorithm(Algorithm.QUICK);
+        return;
+      case "Merge":
+        setAlgorithm(Algorithm.MERGE);
+        return;
+      case "Bubble":
+        setAlgorithm(Algorithm.BUBBLE);
+        return;
+      default:
+        setAlgorithm(Algorithm.BUBBLE)
+    }
   };
 
-  const SortingAlgorithmBtn = ({ algorithm, index }: SortingAlgoBtnProps) => {
-    const buttonHue = index * 100;
-    const algorithmName = algorithm.slice(0, 1) + algorithm.slice(1).toLowerCase();
-    return (
-      <AlgoButton hue={buttonHue} onClick={() => changeAlgo(algorithm)}>
-        {`${algorithmName} Sort`}
-      </AlgoButton>
-    );
-  };
+  const buttonHue = index * 100;
+  return (
+    <AlgoButton hue={buttonHue} onClick={() => changeAlgo(algorithm)}>
+      {`${algorithm} Sort`}
+    </AlgoButton>
+  );
+};
+
+const SortingAlgorithmBtns = ({ setAlgorithm }: SortingAlgoBtnsProps) => {
 
   return (
     <AlgoBtnGroup>
       {SortingAlgorithmsArray.map((sort, idx) => (
-        <SortingAlgorithmBtn key={idx} index={idx} algorithm={sort} />
+        <SortingAlgorithmBtn setAlgorithm={setAlgorithm} key={idx} index={idx} algorithm={sort} />
       ))}
     </AlgoBtnGroup>
   );
